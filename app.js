29201b46b0765ab79ebbcef0a52747b9b382cf7e
Herman Chien <Herman@fet.vue>
2022-04-07 14:55:06 +0800

    component use

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			course: { id: "POOP", name: "Python OOP", duration: 35 },
 			courses: [{ id: "POOP", name: "Python OOP", duration: 35 },
 			{ id: "BDPY", name: "python and big data", duration: 35 }],
 			detailsVisible: true
 		}
 	},
 	computed: {},
+	methods: {
+		toggleCourseDetail() {
+			this.detailsVisible = !this.detailsVisible;
+		}
+	}
+})
+
+app.component("course-content", {
+    template: `
+	<li>
+		<h2>{{course.id}}</h2>
+		<button @click="toggleCourseDetail">Show detail</button>
+		<ul v-if="detailsVisible">
+			<li>{{course.name}}</li>
+			<li>{{course.duration}}</li>
+		</ul>
+	</li>
+	`, data() {
+		return {
+			course: { id: "POOP", name: "Python OOP", duration: 35 },
+			detailsVisible: true
+		}
+	},
 	methods: {
 		toggleCourseDetail() {
 			this.detailsVisible = !this.detailsVisible;
